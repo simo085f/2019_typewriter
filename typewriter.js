e70670bcab1c2d5ff9ea6acbd99600ac0def7970
simo085f <simo085f@stud.kea.dk>
2019-02-11 13:43:49 +0100

    typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,19 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+let text = document.querySelector("#typewriter").textContent;
+let counter = 0;
+let speed = 250;
+
+function init() {
+  document.querySelector("#typewriter").textContent = " ";
+  loop();
+}
+
+function loop() {
+  if (counter < text.length) {
+    document.querySelector("#typewriter").textContent += text.charAt(counter);
+    counter++;
+    setTimeout(loop, speed);
+  }
+}
